db11ab667cefeb80636701e90748a6e1737aa6d1
kennjr <ken.juniour@student.moringaschool.com>
2022-06-16 13:31:54 +0300

    MainCardHead : added html code that'll display the data we need to show and styled the weather data div (overlaying the text on the img

---
+++ src/modules/app/presentation/components/main-card-head/main-card-head/main-card-head.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-card-head',
+  templateUrl: './main-card-head.component.html',
+  styleUrls: ['./main-card-head.component.css']
+})
+export class MainCardHeadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
