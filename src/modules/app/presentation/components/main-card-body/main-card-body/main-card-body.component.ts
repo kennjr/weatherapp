4a273dc9da5800f52fc6f5d15ce7ca3712674d6e
kennjr <ken.juniour@student.moringaschool.com>
2022-06-16 13:32:40 +0300

    MainCardBody : created the component

---
+++ src/modules/app/presentation/components/main-card-body/main-card-body/main-card-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-card-body',
+  templateUrl: './main-card-body.component.html',
+  styleUrls: ['./main-card-body.component.css']
+})
+export class MainCardBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
