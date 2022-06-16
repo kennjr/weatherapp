56b6b96607196000b1ef5d000a2cc6ebc981aeaa
kennjr <ken.juniour@student.moringaschool.com>
2022-06-17 02:35:30 +0300

    AppUtils : Added the base url to the file

---
+++ src/modules/app/common/AppUtils.ts
@@ -0,0 +1,4 @@
+export class AppUtils{
+    private static BASE_URL:string= "http://api.weatherapi.com/v1/";
+    // searchUrl = `https://api.github.com/search/repositories?q=${trimmedSearchString}`;
+}
