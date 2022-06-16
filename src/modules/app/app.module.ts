16ce39cc5bfd5c724305bd2f5c78c905a25468e7
kennjr <ken.juniour@student.moringaschool.com>
2022-06-16 22:45:32 +0300

    AppModule : Added the formsmodule and the commonmodule to the imports array

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainCardHeadComponent } from './presentation/components/main-card-head/main-card-head/main-card-head.component';
+import { MainCardBodyComponent } from './presentation/components/main-card-body/main-card-body/main-card-body.component';
+import { HomeComponent } from './presentation/components/home/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainCardHeadComponent,
+    MainCardBodyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
