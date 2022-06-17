37d8cbeae3504902dbed6212c8f6cc40b12c9a0b
kennjr <ken.juniour@student.moringaschool.com>
2022-06-17 13:44:54 +0300

    AppModule : the weather-data pipe was imported and added to the declarations

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { MainCardHeadComponent } from './presentation/components/main-card-head/main-card-head/main-card-head.component';
 import { MainCardBodyComponent } from './presentation/components/main-card-body/main-card-body/main-card-body.component';
 import { HomeComponent } from './presentation/components/home/home/home.component';
+import { WeatherDataPipe } from './domain/pipes/weather-data/weather-data.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainCardHeadComponent,
     MainCardBodyComponent,
-    HomeComponent
+    HomeComponent,
+    WeatherDataPipe
   ],
   imports: [
     BrowserModule,
