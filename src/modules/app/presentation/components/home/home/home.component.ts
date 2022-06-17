df5b1b5ce6711adcd6fd2054c8e7011f02f3c5f7
kennjr <ken.juniour@student.moringaschool.com>
2022-06-17 15:03:35 +0300

    HomeTs : Wrote code that'll make a request if the user's search string is valid

--- src/modules/app/presentation/components/home/home/home.component.ts
+++ src/modules/app/presentation/components/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppRepo } from 'src/modules/app/data/repository/AppRepo';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,9 @@ export class HomeComponent implements OnInit {
 
   search_string!:string;
 
-  constructor() { }
+  
+
+  constructor(private repo: AppRepo) { }
 
   ngOnInit(): void {
   }
@@ -17,7 +20,29 @@ export class HomeComponent implements OnInit {
   key_has_been_pressed(key :string){
     // check if the pressed key is Enter
     if(key == "Enter"){
-      console.log("The search str ", this.search_string.trim())
+      if (this.search_string != null && this.search_string.trim() != ""){
+        this.repo.get_current_weather_of_city(this.search_string).then(
+          (response: any) => {
+            // check what type of response we got
+            let js_response = JSON.parse(JSON.stringify(response));
+            let response_location = response['location']
+            let response_error = response['error']
+            let response_current = response['current']
+
+            if(response_error != null){
+              // the request wasn't successful
+              // TODO figure out what to do (probably show the user old data or a msg)
+            }else{
+              // the request was successful
+
+            }
+          }, 
+          // the reason for the request rejection
+          (reason: any) => {})
+      }
+      else{
+        // TODO show some sort of snackbar that appears from the bottom or bottom-left
+      }
     }
   }
 }
