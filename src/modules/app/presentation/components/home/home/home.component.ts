cc33ff60e3b84db0373978c173f381e4dc236d72
kennjr <ken.juniour@student.moringaschool.com>
2022-06-17 13:22:39 +0300

    Home : styled the option btns at the bottom after adding them to the template

--- src/modules/app/presentation/components/home/home/home.component.ts
+++ src/modules/app/presentation/components/home/home/home.component.ts
@@ -7,12 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  search_string!:string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  key_has_been_pressed(k :string){
-
+  key_has_been_pressed(key :string){
+    // check if the pressed key is Enter
+    if(key == "Enter"){
+      console.log("The search str ", this.search_string.trim())
+    }
   }
 }
