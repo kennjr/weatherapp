547469c9531d0e890d1bf935aa0441eb995b01e8
kennjr <ken.juniour@student.moringaschool.com>
2022-06-18 12:01:49 +0300

    MainCardBody : Added the inputs in the component class and used interpolation to show the appropriate val.s in the template

--- src/modules/app/presentation/components/main-card-body/main-card-body/main-card-body.component.ts
+++ src/modules/app/presentation/components/main-card-body/main-card-body/main-card-body.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { WeatherCurrent, WeatherLocation } from 'src/modules/app/data/model/weather';
 
 @Component({
   selector: 'app-main-card-body',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-card-body.component.css']
 })
 export class MainCardBodyComponent implements OnInit {
+
+  @Input() current_location!: WeatherLocation;
+  @Input() current_weather!: WeatherCurrent;
 
   constructor() { }
 
