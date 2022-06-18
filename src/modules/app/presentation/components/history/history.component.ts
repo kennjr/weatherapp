259fdb03cab888b1798969cf48a4eeada008e4e1
kennjr <ken.juniour@student.moringaschool.com>
2022-06-19 02:30:47 +0300

    HistoryComponent : added the ondestroy method, added the appreo to the constructor and created a fun that'll get all the user's history from the db

--- src/modules/app/presentation/components/history/history.component.ts
+++ src/modules/app/presentation/components/history/history.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { WeatherCurrent, WeatherLocation } from 'src/modules/app/data/model/weather';
 import { AppRepo } from 'src/modules/app/data/repository/AppRepo';
 
@@ -7,16 +8,38 @@ import { AppRepo } from 'src/modules/app/data/repository/AppRepo';
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.css']
 })
-export class HistoryComponent implements OnInit {
+export class HistoryComponent implements OnInit, OnDestroy {
 
   search_string!: string;
   
   search_weather_history!: WeatherCurrent[]
   search_location_history!: WeatherLocation[]
 
+  weather_history_subscription!: Subscription;
+  location_history_subscription!: Subscription;
+
   constructor(private repo: AppRepo) { }
 
   ngOnInit(): void {
+    // make a request for the items, so that we can populate the list
+    this.get_all_weather_history()
+  }
+
+  ngOnDestroy(): void {
+    // best practice
+    this.weather_history_subscription.unsubscribe();
+    this.location_history_subscription.unsubscribe();
+  }
+
+  private get_all_weather_history(){
+    // init the sub. var we created
+    this.weather_history_subscription = this.repo.get_all_weather_history().subscribe({
+      next: ((value: WeatherCurrent[]) => {
+        this.search_weather_history = value;
+      }), error: ((error: any) => {
+        // TODO figure out how to show an error msg to the user
+        console.log("We got an error ", error.toString());
+      })})
   }
 
   key_has_been_pressed(key :string){
