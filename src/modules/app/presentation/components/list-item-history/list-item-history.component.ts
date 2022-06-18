1223da2c193552c9daa7310e9aa25cb8c3763729
kennjr <ken.juniour@student.moringaschool.com>
2022-06-19 01:15:10 +0300

    ListItemHistory : created the component and added its structure to the template then styled it

---
+++ src/modules/app/presentation/components/list-item-history/list-item-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-item-history',
+  templateUrl: './list-item-history.component.html',
+  styleUrls: ['./list-item-history.component.css']
+})
+export class ListItemHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
