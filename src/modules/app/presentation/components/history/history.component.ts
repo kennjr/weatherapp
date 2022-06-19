e9b7be59db18d48a71474ca5e3f31c317fad014b
kennjr <ken.juniour@student.moringaschool.com>
2022-06-19 22:06:25 +0300

    HistoryComponent : created the fun.s needed for navigating the user back to the prev. page

--- src/modules/app/presentation/components/history/history.component.ts
+++ src/modules/app/presentation/components/history/history.component.ts
@@ -1,5 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { WeatherCurrentDto } from 'src/modules/app/data/dto/WeatherCurrentDto';
+import { WeatherLocationDto } from 'src/modules/app/data/dto/WeatherLocationDto';
 import { WeatherCurrent, WeatherLocation } from 'src/modules/app/data/model/weather';
 import { AppRepo } from 'src/modules/app/data/repository/AppRepo';
 
@@ -18,11 +22,12 @@ export class HistoryComponent implements OnInit, OnDestroy {
   weather_history_subscription!: Subscription;
   location_history_subscription!: Subscription;
 
-  constructor(private repo: AppRepo) { }
+  constructor(private repo: AppRepo, private location: Location) { }
 
   ngOnInit(): void {
     // make a request for the items, so that we can populate the list
     this.get_all_weather_history()
+    this.get_all_locations_history()
   }
 
   ngOnDestroy(): void {
@@ -42,6 +47,18 @@ export class HistoryComponent implements OnInit, OnDestroy {
       })})
   }
 
+  private get_all_locations_history(){
+    // init the sub. var we created
+    this.location_history_subscription = this.repo.get_all_location_history().subscribe({
+      next: ((value: WeatherLocation[]) => {
+        this.search_location_history = value;
+        console.log("The response ", value)
+      }), error: ((error: any) => {
+        // TODO figure out how to show an error msg to the user
+        console.log("We got an error ", error);
+      })})
+  }
+
   key_has_been_pressed(key :string){
     console.log("The key ", key)
     // check if the pressed key is Enter
@@ -53,6 +70,14 @@ export class HistoryComponent implements OnInit, OnDestroy {
         // TODO show some sort of snackbar that appears from the bottom or bottom-left
       }
     }
+  }
+
+  on_back_pressed(){
+    this.navigate_to_prev_page()
+  }
+
+  private navigate_to_prev_page(){
+    this.location.back()
   }
 
 }
