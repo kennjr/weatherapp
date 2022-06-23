da7e906f00d8934b56fc416bd8d134ba66491836
kennjr <ken.juniour@student.moringaschool.com>
2022-06-23 22:10:21 +0300

    ListItemHistoryComponent : added the fun. for deleting an item

--- src/modules/app/presentation/components/list-item-history/list-item-history.component.ts
+++ src/modules/app/presentation/components/list-item-history/list-item-history.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { WeatherCurrent, WeatherLocation } from 'src/modules/app/data/model/weather';
+import { AppRepo } from 'src/modules/app/data/repository/AppRepo';
 
 @Component({
   selector: 'app-list-item-history',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListItemHistoryComponent implements OnInit {
 
-  constructor() { }
+  @Input() location!: any;
+  @Input() weather!: any;
+
+  @Output() on_delete_weather_item: EventEmitter<WeatherCurrent> = new EventEmitter()
+  @Output() on_delete_location_item: EventEmitter<WeatherLocation> = new EventEmitter()
+
+  constructor(private repo: AppRepo) { }
 
   ngOnInit(): void {
+    
   }
 
+  on_delete_clicked(){
+    this.delete_item()
+  }
+
+  private delete_item(){
+    console.log("Delete clicked ")
+    this.on_delete_location_item.emit(this.location);
+    this.on_delete_weather_item.emit(this.weather);
+  }
 }
