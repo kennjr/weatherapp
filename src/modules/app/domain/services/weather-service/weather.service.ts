48a34cbefbf742744813536582faa95386732ff4
kennjr <ken.juniour@student.moringaschool.com>
2022-06-26 13:12:12 +0300

    WeatherService : created 2 fun.s that'll set and get the recently searched city str form the localStorage

--- src/modules/app/domain/services/weather-service/weather.service.ts
+++ src/modules/app/domain/services/weather-service/weather.service.ts
@@ -17,6 +17,23 @@ export class WeatherService {
     return lastValueFrom(this.http_client.get(query_url));
   }
 
+  // we store the string so that we can make a new req. when the user comes back to the app
+  store_recent_search_string(search_str: string){
+    if(search_str.trim() != ""){
+      localStorage.setItem(AppUtils.RECENTLY_SEARCHED_CITY_KEY, search_str);
+    }
+  }
+
+  // this will give us the city the user search for (most recently), so that we can make an api req for that city
+  get_recent_search_string(): string{
+    let str = localStorage.getItem(AppUtils.RECENTLY_SEARCHED_CITY_KEY)
+    if (str){
+      return str;
+    }else{
+      return AppUtils.DEFAULT_CITY
+    }
+  }
+
   private construct_url(city_str: string): string{
 
     let url = `${AppUtils.CURRENT_WEATHER_URL}key=${environment.api_key}&q=${city_str}`;
