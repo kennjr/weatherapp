225a859b098b6ee100aea95e8dd28b4a7c0e0a9f
kennjr <ken.juniour@student.moringaschool.com>
2022-06-29 00:18:24 +0300

    AppRoutingModule : commented out the settings route

--- src/modules/app/app-routing.module.ts
+++ src/modules/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryComponent } from './presentation/components/history/history.component';
 import { HomeComponent } from './presentation/components/home/home/home.component';
+import { SettingsComponent } from './presentation/components/settings/settings.component';
 
 const routes: Routes = [
-  {path: '', component:HomeComponent}
+  { path: '', component: HomeComponent },
+  // { path: 'settings', component:SettingsComponent },
+  { path: 'history', component: HistoryComponent }
 ];
 
 @NgModule({
