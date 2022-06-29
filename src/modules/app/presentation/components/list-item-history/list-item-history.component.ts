24a6d8ad342f320fed56deffc27b357d3cb730c5
kennjr <ken.juniour@student.moringaschool.com>
2022-06-30 01:54:09 +0300

    ListItemHistoryTs : created the output var. and created the fun that emits it's val when the click occurs in the template

--- src/modules/app/presentation/components/list-item-history/list-item-history.component.ts
+++ src/modules/app/presentation/components/list-item-history/list-item-history.component.ts
@@ -14,6 +14,7 @@ export class ListItemHistoryComponent implements OnInit {
   @Input() key!: string;
 
   @Output() on_delete_item: EventEmitter<string> = new EventEmitter()
+  @Output() on_click_item: EventEmitter<string> = new EventEmitter()
 
   constructor(private repo: AppRepo) { }
 
@@ -24,8 +25,16 @@ export class ListItemHistoryComponent implements OnInit {
     this.delete_item()
   }
 
+  on_item_clicked(){
+    this.click_item()
+  }
+
   private delete_item(){
     console.log("Delete clicked ")
     this.on_delete_item.emit(this.key);
+  }
+
+  private click_item(){
+    this.on_click_item.emit(this.key)
   }
 }
