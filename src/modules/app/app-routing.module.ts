e1894b0bc3ed07b8c592fc807d2350c38a159971
kennjr <ken.juniour@student.moringaschool.com>
2022-06-30 12:30:36 +0300

    AppRoutingModule : intro'd the historyItemFull component to the routes

--- src/modules/app/app-routing.module.ts
+++ src/modules/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HisotryItemFullComponent } from './presentation/components/hisotry-item-full/hisotry-item-full.component';
 import { HistoryComponent } from './presentation/components/history/history.component';
 import { HomeComponent } from './presentation/components/home/home/home.component';
 import { SettingsComponent } from './presentation/components/settings/settings.component';
@@ -7,7 +8,8 @@ import { SettingsComponent } from './presentation/components/settings/settings.c
 const routes: Routes = [
   { path: '', component: HomeComponent },
   // { path: 'settings', component:SettingsComponent },
-  { path: 'history', component: HistoryComponent }
+  { path: 'history', component: HistoryComponent},
+  { path: 'history/:key', component: HisotryItemFullComponent},
 ];
 
 @NgModule({
